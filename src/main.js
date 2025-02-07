16d4489059fc26ce0e19760677d959234817584b
EugeoDev <littleugeod@gmail.com>
2025-02-06 21:33:09 -0400

    html: updated, css: completed, js: finished

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 import './style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapperForms = document.querySelector('.wrapper__forms');
+    const formLogin = document.querySelector('.form__login');
+    const formRegister = document.querySelector('.form__register');
+    const btnRegisterInformation = document.getElementById('btn-register-information');
+    const btnLoginInformation = document.getElementById('btn-login-information');
+
+    formLogin.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+
+    formRegister.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+
+    btnRegisterInformation.addEventListener('click', () => {
+        wrapperForms.classList.add('registering');
+        wrapperForms.classList.add('active-editing');
+        formLogin.classList.add('active-editing');
+        formRegister.classList.add('active-editing');
+    });
+
+    btnLoginInformation.addEventListener('click', () => {
+        wrapperForms.classList.remove('registering');
+    });
+});
